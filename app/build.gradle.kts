f182d913bb09639216e8d78c69656815b5339cf6
shenben <714081644@qq.com>
2024-06-24 14:15:00 +0800

    完善demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(project(":lib"))
+//    implementation(libs.wechatkit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
